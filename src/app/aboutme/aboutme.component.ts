abc4f006b3a18f7c9c27e48005769fec2f0c5485
asmabouchoucha <moukamouka1234@gmail.com>
2019-01-03 20:23:47 +0100

    added the "mastred technologies" to the about me section

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.scss']
+  styleUrls: ['./aboutme.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        opacity: 1,
+      })),
+      state('closed', style({
+        opacity: 0,
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('1s')
+      ]),
+    ]),
+  ],
 })
 export class AboutmeComponent implements OnInit {
-
+ isBioDisplayed = true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  displayBio() {
+    this.isBioDisplayed = !this.isBioDisplayed;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ComponentName} from '../app.component';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  @Output() onComponentSelected = new EventEmitter<ComponentName>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  selectComponent(component: ComponentName) {
+    this.onComponentSelected.emit(component);
+  }
 }
