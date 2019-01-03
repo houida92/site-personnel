44958b9591f04ec0533daf15ad7b948886f68476
asmabouchoucha <moukamouka1234@gmail.com>
2019-01-03 14:03:34 +0100

    connected the icons "home" and "aboutme" to their respective components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+export type ComponentName = "about" | "home" | "resume";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'site-personnel';
+  selectedComponent: ComponentName = 'home';
+  getSelectedComponent(selectedComponent: ComponentName) {
+    console.log("selected component:", selectedComponent)
+    this.selectedComponent = selectedComponent;
+  }
+
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ComponentName} from '../app.component';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() onComponentSelected = new EventEmitter<ComponentName>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+  selectComponent(component: ComponentName) {
+    this.onComponentSelected.emit(component);
   }
 
 }
