08fc8d66b7eb214cc13f7b23a74f5dac0d3ebdd5
asmabouchoucha <moukamouka1234@gmail.com>
2019-01-07 01:15:49 +0100

    HomeComponent - added animation to the text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ComponentName} from '../app.component';
+import {animate, keyframes, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('appear', [
+
+      transition('void => *',
+        animate(1000,
+          keyframes([
+            style({opacity: 0, offset: 0}),
+            style({opacity: 0.5 , offset: 0.5}),
+            style({ opacity: 1, offset: 1})
+          ])))
+    ])
+  ],
 })
 export class HomeComponent implements OnInit {
   @Output() onComponentSelected = new EventEmitter<ComponentName>();
